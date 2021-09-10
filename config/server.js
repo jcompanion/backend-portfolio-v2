feaee45d890c9042253df98ce0c9a227d3777b64
jcompanion <joshua.companion@g.austincc.edu>
2021-09-10 11:06:06 -0500

    Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ddb2050a591869d12f4bf48b0accba65'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13d9059d-b02d-4508-922f-bed99cab81f0'
+};
